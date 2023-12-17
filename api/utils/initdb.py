29d7b56a163def48b6d504d7e9fd263e7e864264
lineinna <waskevin1125@gmail.com>
2023-12-17 16:40:22 +0800

    updateForCheckout

--- api/utils/initdb.py
+++ api/utils/initdb.py
@@ -1,6 +1,7 @@
 from api.models.book import Book
 from api.models.page import Page
 from api.models.user import User
+from api.models.comment import Comment
 from api.utils.time import now
 from api.utils.db import db
 
