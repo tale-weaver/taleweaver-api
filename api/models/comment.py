c01eff739e62428564acb13e368c39c90dfe4f6d
lineinna <waskevin1125@gmail.com>
2023-12-19 13:54:08 +0800

    CommentFinalV

--- api/models/comment.py
+++ api/models/comment.py
@@ -39,34 +39,39 @@ class Comment:
     @staticmethod
     def find_comment_of_book(book_id):
         book_oid = ObjectId(book_id)
-        pipeline = [
-            {"$match": {"_id": book_oid}},
-            {"$unwind": "$comment_ids"},
-            {
-                "$lookup": {
-                    "from": "comment_ids",
-                    "localField": "comment_ids",
-                    "foreignField": "_id",
-                    "as": "comments",
-                }
-            },
-            {"$unwind": "$comments"},
-            {
-                "$project": {
-                    "comments" : "$comments"
-                }
-            },
-        ]
-        result = db.books.aggregate(pipeline)
+        book = db.books.find_one({"_id": book_oid})
+        result=[]
+        for comments in book["comment_ids"]:
+            oid=ObjectId(comments)
+            result.append(db.comments.find_one({"_id": oid}))
+        # pipeline = [
+        #     {"$match": {"_id": book_oid}},
+        #     {"$unwind": "$comment_ids"},
+        #     {
+        #         "$lookup": {
+        #             "from": "comment_ids",
+        #             "localField": "comment_ids",
+        #             "foreignField": "_id",
+        #             "as": "comments",
+        #         }
+        #     },
+        #     {"$unwind": "$comments"},
+        #     {
+        #         "$project": {
+        #             "comments" : "$comments"
+        #         }
+        #     },
+        # ]
+        # result = db.books.aggregate(pipeline)
         comment_list = []
         for comment in result:
-            print(comment)
-            user = db.users.find_one({"_id": ObjectId(comment["comments"]["commenter_id"])})
+            user = db.users.find_one({"_id": ObjectId(comment["commenter_id"])})
             comment_list.append({
                 "username": user['username'],
                 "avatar": user['avatar'],
-                "commenter_id": str(comment["comments"]["commenter_id"]),
-                "image": comment["comments"]["image"],
-                "created_at": comment["comments"]["created_at"]
+                "commenter_id": str(comment["commenter_id"]),
+                "review": comment["review"],
+                "rating": comment["rating"],
+                "created_at": comment["created_at"]
             })
         return comment_list
